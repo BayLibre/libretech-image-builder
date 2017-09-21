d9143f24cc5f57eb2643d7ac74055b781acbc952
Neil Armstrong <narmstrong@baylibre.com>
2017-09-21 17:27:54 +0200

    Fixups stage2

--- stage2.sh
+++ stage2.sh
@@ -14,14 +14,17 @@ echo "libre:computer" | chpasswd
 adduser libre sudo
 apt-get update
 apt-get -y dist-upgrade
-apt-get install -y vim build-essential git xubuntu-desktop
+apt-get install -y vim build-essential git
+apt-get install -y libtool automake autoconf xutils-dev xserver-xorg-dev xorg-dev libudev-dev
+apt-get install -y xubuntu-desktop
 
 # disable mesa EGL libs
 rm /etc/ld.so.conf.d/*_EGL.conf
 ldconfig
 
-git clone git@github.com:superna9999/xf86-video-armsoc.git -b meson-drm
+git clone https://github.com/superna9999/xf86-video-armsoc.git -b meson-drm
 cd xf86-video-armsoc
+./autogen.sh
 ./configure
 make install
 mkdir -p /etc/X11
