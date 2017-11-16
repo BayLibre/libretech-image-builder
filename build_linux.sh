1f2dee4d2e3d1057fa32696cd042467639eff934
Jerome Brunet <jbrunet@baylibre.com>
2017-11-16 17:56:27 +0100

    Revert "Revert "Add Meson DRM patch""
    This reverts commit d18c15f80ebc6d628b47371fe6ad664bc916c3a9.

--- build_linux.sh
+++ build_linux.sh
@@ -1,6 +1,7 @@
 git clone https://github.com/libre-computer-project/libretech-linux.git -b linux-4.14/libretech-cc-master --depth 1 linux-libretech
 cd linux-libretech
 make ARCH=arm64 defconfig
+git apply ../patches/0001-meson_drm_disable_ARGB8888.patch
 sed -i 's/CONFIG_BTRFS_FS=m/CONFIG_BTRFS_FS=y/' .config
 PATH=$PWD/../gcc-linaro-6.4.1-2017.08-x86_64_aarch64-linux-gnu/bin:$PATH make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- -j4
 cd -
