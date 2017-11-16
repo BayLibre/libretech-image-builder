47674dd426204d1f45a65f39b26769b8dfee7b3e
Jerome Brunet <jbrunet@baylibre.com>
2017-11-16 16:28:08 +0100

    Change kernel branch and config
    the new master branch is libretech-cc-master and config is defconfig
    
    Signed-off-by: Jerome Brunet <jbrunet@baylibre.com>

--- build_linux.sh
+++ build_linux.sh
@@ -1,5 +1,5 @@
-git clone https://github.com/libre-computer-project/libretech-linux.git -b linux-4.13/libretech-cc-overlays --depth 1 linux-libretech
+git clone https://github.com/libre-computer-project/libretech-linux.git -b linux-4.14/libretech-cc-master --depth 1 linux-libretech
 cd linux-libretech
-make ARCH=arm64 meson_defconfig
+make ARCH=arm64 defconfig
 PATH=$PWD/../gcc-linaro-6.4.1-2017.08-x86_64_aarch64-linux-gnu/bin:$PATH make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- -j4
 cd -
