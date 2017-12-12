023a9a05a87269fea457e2b98dffbc353f3082f1
Jerome Brunet <jbrunet@baylibre.com>
2017-12-12 15:08:24 +0100

    Use stable kernel patches now
    Signed-off-by: Jerome Brunet <jbrunet@baylibre.com>

--- build_linux.sh
+++ build_linux.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/libre-computer-project/libretech-linux.git -b linux-4.14/libretech-cc-master --depth 1 linux-libretech
+git clone https://github.com/libre-computer-project/libretech-linux.git -b linux-4.14/libretech-cc-master-stable --depth 1 linux-libretech
 cd linux-libretech
 make ARCH=arm64 defconfig
 sed -i 's/CONFIG_BTRFS_FS=m/CONFIG_BTRFS_FS=y/' .config
